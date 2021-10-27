bcc688ecf7a85d7f9bb4fd8c8cbd11daaa8f4ed9
polunzh <polunzh@gmail.com>
2021-10-27 18:44:13 +0800

    clean useless codes

--- src/components/instance/list.js
+++ src/components/instance/list.js
@@ -1,66 +1,15 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { LocaleContext } from '@arcblock/ux/lib/Locale/context';
-import { formatToDatetime } from '@arcblock/ux/lib/Util';
-import DIDAddress from '@arcblock/did-connect/lib/Address';
 
-import Table from '../table';
-
-export default function List({ actionColumn, abtnodes, ...props }) {
-  const { t } = useContext(LocaleContext);
-
-  const options = {
-    print: false,
-    download: false,
-    filter: false,
-    sort: false,
-    selectableRows: 'single',
-    selectToolbarPlacement: 'none',
-    selectableRowsOnClick: true,
-    selectableRowsHideCheckboxes: true,
-    filterType: 'dropdown',
-    responsive: 'vertical',
-    pagination: false,
-  };
-
-  const columns = [
-    {
-      name: 'name',
-      label: t('common.name'),
-    },
-    {
-      name: 'did',
-      label: 'DID',
-      options: {
-        customBodyRender: (value) => <DIDAddress>{value}</DIDAddress>,
-      },
-    },
-    {
-      name: 'url',
-      label: 'URL',
-    },
-    {
-      name: 'launchedAt',
-      label: t('common.launchedAt'),
-      options: {
-        customBodyRender: (value) => formatToDatetime(value),
-      },
-    },
-  ];
-
-  if (actionColumn) {
-    columns.push(actionColumn);
-  }
-
-  return <Table {...props} data={abtnodes} columns={columns} options={options} />;
+export default function List({ abtnodes }) {
+  console.log(abtnodes);
+  return <div>测试</div>;
 }
 
 List.propTypes = {
-  actionColumn: PropTypes.object,
   abtnodes: PropTypes.arrayOf(PropTypes.object),
 };
 
 List.defaultProps = {
-  actionColumn: null,
   abtnodes: [],
 };
