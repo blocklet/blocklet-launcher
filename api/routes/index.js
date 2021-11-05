6ab5b3fe62018a7a88d250670d33e0808662812e
polunzh <polunzh@gmail.com>
2021-11-05 10:50:29 +0800

    fix some ui issues and fetch blocklet meta bug

---
+++ api/routes/index.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+module.exports = {
+  init(app) {
+    app.get('/api/blocklet-meta', async (req, res) => {
+      let blockletMetaUrl = req.query.url;
+      if (!blockletMetaUrl) {
+        return res.status(400).json({ error: 'url param is required' });
+      }
+
+      blockletMetaUrl = decodeURIComponent(blockletMetaUrl);
+
+      const { data: meta } = await axios.get(blockletMetaUrl);
+
+      return { meta };
+    });
+  },
+};
--- src/libs/api.js
+++ src/libs/api.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
+import joinUrl from 'url-join';
 
 axios.defaults.timeout = 200000;
 axios.defaults.withCredentials = false;
 axios.defaults.crossDomain = true;
 
+axios.interceptors.request.use((config) => {
+  const prefix = window.blocklet ? window.blocklet.prefix : '';
+  config.baseURL = prefix || '';
+  config.url = joinUrl('/api', config.url);
+
+  return config;
+});
 export default axios;
