7b6f990f0d0e13fd46b8d8a70973dda260d6edd1
polunzh <polunzh@gmail.com>
2021-11-07 15:07:41 +0800

    [mobile] page header

---
+++ src/hooks/is-mobile.js
@@ -0,0 +1,7 @@
+import { useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+
+export default function useMobile() {
+  const theme = useTheme();
+  return useMediaQuery(theme.breakpoints.down('sm'));
+}
