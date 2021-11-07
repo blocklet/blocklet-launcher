dedfce8147e58101fd5c51829951977e216e45b8
polunzh <polunzh@gmail.com>
2021-11-07 15:07:41 +0800

    [mobile] page header

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -9,11 +9,12 @@ import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
-import { Done } from '@material-ui/icons';
-import { Link, Paper } from '@material-ui/core';
+import { Done as DoneIcon, Menu as MenuIcon } from '@material-ui/icons';
+import { Hidden, Link, Paper } from '@material-ui/core';
 import { useBlockletMetaContext } from '../../libs/context/blocklet-meta';
 import AppHeader from '../app-header';
 import { getBlockletLogoUrl } from '../../libs/utils';
+import useMobile from '../../hooks/is-mobile';
 
 function CustomStepIcon({ active, completed }) {
   const urlParams = new URLSearchParams(window.location.search);
@@ -24,7 +25,7 @@ function CustomStepIcon({ active, completed }) {
   });
 
   if (completed) {
-    return <Done style={{ color: '#31AB86' }} />;
+    return <DoneIcon style={{ color: '#31AB86' }} />;
   }
 
   if (active) {
@@ -39,46 +40,74 @@ CustomStepIcon.propTypes = {
   completed: PropTypes.bool.isRequired,
 };
 
+const Div = styled.div``;
+
 function Layout({ children }) {
   const { t } = useLocaleContext();
 
   const blockletMeta = useBlockletMetaContext();
+  const isMobile = useMobile();
+
+  const Container = isMobile ? Div : Paper;
 
   return (
-    <Div>
-      <div className="root-header">
-        <LocaleSelector size={26} showText={false} className="locale-addon" />
-      </div>
-      <Paper className="box">
-        <div className="nav-sidebar">
-          <AppHeader
-            title={blockletMeta.data.title}
-            subTitle={
-              // eslint-disable-next-line react/jsx-wrap-multilines
-              <Link target="_blank" href={joinUrl(blockletMeta.registryUrl, `/blocklet/${blockletMeta.data.did}`)}>
-                {t('launch.openInRegistry')}
-              </Link>
-            }
-            logoUrl={getBlockletLogoUrl({
-              did: blockletMeta.data.did,
-              baseUrl: blockletMeta.registryUrl,
-              logoPath: blockletMeta.data.logo,
-            })}
-          />
-          <Stepper className="stepper" activeStep={0} orientation="vertical">
-            <Step key="select-node">
-              <StepLabel StepIconComponent={CustomStepIcon}>{t('launch.selectAbtNode')}</StepLabel>
-            </Step>
-            <Step key="launch-app">
-              <StepLabel StepIconComponent={CustomStepIcon}>{t('launch.launchApp')}</StepLabel>
-            </Step>
-          </Stepper>
+    <Root>
+      <header className="root-header">
+        <Hidden smUp>
+          <div className="left">
+            <MenuIcon className="menu__icon" />
+            <AppHeader
+              title={blockletMeta.data.title}
+              subTitle={
+                // eslint-disable-next-line react/jsx-wrap-multilines
+                <Link target="_blank" href={joinUrl(blockletMeta.registryUrl, `/blocklet/${blockletMeta.data.did}`)}>
+                  {t('launch.openInRegistry')}
+                </Link>
+              }
+              logoUrl={getBlockletLogoUrl({
+                did: blockletMeta.data.did,
+                baseUrl: blockletMeta.registryUrl,
+                logoPath: blockletMeta.data.logo,
+              })}
+            />
+          </div>
+        </Hidden>
+        <div className="right">
+          <LocaleSelector size={26} showText={false} className="locale-addon" />
         </div>
+      </header>
+      <Container className="box">
+        <Hidden smDown>
+          <div className="nav-sidebar">
+            <AppHeader
+              title={blockletMeta.data.title}
+              subTitle={
+                // eslint-disable-next-line react/jsx-wrap-multilines
+                <Link target="_blank" href={joinUrl(blockletMeta.registryUrl, `/blocklet/${blockletMeta.data.did}`)}>
+                  {t('launch.openInRegistry')}
+                </Link>
+              }
+              logoUrl={getBlockletLogoUrl({
+                did: blockletMeta.data.did,
+                baseUrl: blockletMeta.registryUrl,
+                logoPath: blockletMeta.data.logo,
+              })}
+            />
+            <Stepper className="stepper" activeStep={0} orientation="vertical">
+              <Step key="select-node">
+                <StepLabel StepIconComponent={CustomStepIcon}>{t('launch.selectAbtNode')}</StepLabel>
+              </Step>
+              <Step key="launch-app">
+                <StepLabel StepIconComponent={CustomStepIcon}>{t('launch.launchApp')}</StepLabel>
+              </Step>
+            </Stepper>
+          </div>
+        </Hidden>
         <div id="content" className="content">
           {children}
         </div>
-      </Paper>
-    </Div>
+      </Container>
+    </Root>
   );
 }
 
@@ -86,30 +115,64 @@ Layout.propTypes = {
   children: PropTypes.object.isRequired,
 };
 
-const Div = styled.div`
+const Root = styled.div`
   height: 100vh;
-  background: #e5e5e5;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 
+  ${(props) => props.theme.breakpoints.up('sm')} {
+    background: #e5e5e5;
+  }
+
+  ${(props) => props.theme.breakpoints.down('sm')} {
+    background: #ffffff;
+  }
+
   .root-header {
+    display: flex;
+    align-items: center;
     position: fixed;
     display: flex;
-    justify-content: flex-end;
     top: 0;
     width: 100%;
     padding: 24px;
+
+    ${(props) => props.theme.breakpoints.down('sm')} {
+      justify-content: space-between;
+    }
+
+    ${(props) => props.theme.breakpoints.up('sm')} {
+      justify-content: flex-end;
+    }
+
+    .left {
+      display: flex;
+      align-items: center;
+
+      .menu__icon {
+        margin-right: 18px;
+        color: #999999;
+      }
+    }
+
+    .right {
+      display: flex;
+      align-items: center;
+    }
   }
 
   .box {
     display: flex;
-    width: 80%;
-    max-width: 1245px;
-    height: 80%;
-    max-height: 880px;
     overflow: auto;
+
+    ${(props) => props.theme.breakpoints.up('sm')} {
+      width: 80%;
+      max-width: 1245px;
+      height: 80%;
+      max-height: 880px;
+    }
   }
 
   .content {
