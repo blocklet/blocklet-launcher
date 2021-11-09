cd6cf67ca8ac59a682ff218d5dea88d3fcf2e9ad
polunzh <polunzh@gmail.com>
2021-11-09 15:23:35 +0800

    [mobile] create new node page and node list page

--- src/pages/new-node.js
+++ src/pages/new-node.js
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Spinner from '@arcblock/ux/lib/Spinner';
 import useQuery from '../hooks/query';
 import { getBlockletMetaUrl, getEnvironment } from '../libs/utils';
 
 export default function NewNode() {
   const query = useQuery();
+  const ref = useRef();
   const [loading, setLoading] = useState(true);
+  const [width, setWidth] = useState('0');
+  const [height, setHeight] = useState('0');
 
   const blockletMetaUrl = getBlockletMetaUrl(query);
 
@@ -15,14 +18,20 @@ export default function NewNode() {
 
   const handleLoaded = () => setLoading(false);
 
+  useEffect(() => {
+    const rect = ref.current.parentElement.getBoundingClientRect();
+    setWidth(`${rect.width}px`);
+    setHeight(`${rect.height - 10}px`);
+  }, []);
+
   return (
-    <div>
+    <div ref={ref}>
       {loading && <Spinner />}
       <iframe
         onLoad={handleLoaded}
         title="purchase abt node"
-        width="800px"
-        height="800px"
+        width={width}
+        height={height}
         frameBorder="0"
         border="0"
         cellSpacing="0"
