d539a065ffb0dde83d96c3d0423e60735a627da4
polunzh <polunzh@gmail.com>
2021-12-13 18:27:52 +0800

    update ci:
    - no longer deploy to aliyun blocklet server
    - no longer publish to npm

--- tools/publish.sh
+++ tools/publish.sh
@@ -3,15 +3,8 @@ set -e
 VERSION=$(cat version | awk '{$1=$1;print}')
 echo "publish version ${VERSION}"
 
-git config --local user.name "bot"
-git config --local user.email "bot@arcblock.io"
-
-npm config set '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}"
-
-echo "publishing to npm..."
 echo "SKIP_PREFLIGHT_CHECK=true" > .env
 npm run build
-# npm publish .blocklet/bundle
 
 make release
 
@@ -19,16 +12,7 @@ make release
 set +e
 NAME=$(cat package.json | grep name | head -n 1 |  awk '{print $2}' | sed 's/"//g' | sed 's/,//g')
 VERSION=$(cat package.json | grep version | head -n 1 |  awk '{print $2}' | sed 's/"//g' | sed 's/,//g')
-if [ "${ALIYUN_NODE_ENDPOINT}" != "" ]; then
-  blocklet deploy .blocklet/bundle --endpoint ${ALIYUN_NODE_ENDPOINT} --access-key ${ALIYUN_NODE_ACCESS_KEY} --access-secret ${ALIYUN_NODE_ACCESS_SECRET} --skip-hooks
-  if [ $? == 0 ]; then
-    echo "deploy to ${ALIYUN_NODE_ENDPOINT} success"
-    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${NAME} v${VERSION} was successfully deployed to ${ALIYUN_NODE_ENDPOINT}\"}" ${SLACK_WEBHOOK}
-  else
-    echo "deploy to ${ALIYUN_NODE_ENDPOINT} failed"
-    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\":x: Faild to deploy ${NAME} v${VERSION} to ${ALIYUN_NODE_ENDPOINT}\"}" ${SLACK_WEBHOOK}
-  fi
-fi
+
 if [ "${AWS_NODE_ENDPOINT}" != "" ]; then
   blocklet deploy .blocklet/bundle --endpoint ${AWS_NODE_ENDPOINT} --access-key ${AWS_NODE_ACCESS_KEY} --access-secret ${AWS_NODE_ACCESS_SECRET} --skip-hooks
   if [ $? == 0 ]; then
