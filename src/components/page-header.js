dd6e0bee914c4f9c49809b8edfadcaf4402a12ac
YAO <kirayao@hotmail.com>
2022-01-13 23:45:45 +0800

    fix: change page-header margin top

--- src/components/page-header.js
+++ src/components/page-header.js
@@ -12,6 +12,9 @@ function PageHeader({ title, subTitle }) {
 }
 
 const Content = styled.div`
+  ${(props) => props.theme.breakpoints.up('md')} {
+    margin-top: 36px;
+  }
   text-align: center;
 
   .title {
